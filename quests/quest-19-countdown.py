99cd055519257b02d6b7782958acfe66590dc382
Becca4375 <adhiamborebecca95@gmail.com>
2026-04-15 22:21:41 +0400

    quest 19 countdown

---
+++ quests/quest-19-countdown.py
@@ -0,0 +1,8 @@
+print("Rocket launch sequence initiated...")
+
+# Countdown from 10 to 1 using range with step -1
+for i in range(10, 0, -1):
+    print(i)
+
+# After the loop finishes, print the final message
+print("Blastoff!")
