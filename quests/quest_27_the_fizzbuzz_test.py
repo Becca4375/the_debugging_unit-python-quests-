08cd409b5bf47f1658d9692cc1ddcc65916743f0
Lovella.ntl <l.nkusi@alustudent.com>
2026-04-15 22:40:31 +0400

    Added quest_27

---
+++ quests/quest_27_the_fizzbuzz_test.py
@@ -0,0 +1,10 @@
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buz")
+    else:
+        print(i)
+        
