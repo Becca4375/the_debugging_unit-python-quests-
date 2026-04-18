ed3aa4d207b0876f5b0728dec68c4cec344bf3a9
Becca4375 <r.adhiamboo@alustudent.com>
2026-04-18 12:34:36 +0400

    Update quest_03_treasure_chest.py

---
+++ quests/quest_03_treasure_chest.py
@@ -0,0 +1,8 @@
+# Quest 3: The Treasure Chest
+
+# Create variables with different data types
+age = 19          # integer
+favorite_food = "pizza"   # string
+
+# Print them in one sentence
+print(f"I am {age} years old and my favorite food is {favorite_food}.")
