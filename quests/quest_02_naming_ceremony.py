f698b1e5998e0f94a48c9c7f69b389424dd4d2d3
Becca4375 <r.adhiamboo@alustudent.com>
2026-04-18 12:33:56 +0400

    Update quest_02_naming_ceremony.py

---
+++ quests/quest_02_naming_ceremony.py
@@ -0,0 +1,8 @@
+# Quest 2: The Naming Ceremony
+
+# Create variables
+hero_name = "Astra"
+superpower = "invisibility"
+
+# Print introduction
+print("The hero " + hero_name + " has the power of " + superpower + ".")
